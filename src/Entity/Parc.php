5e9ca882a0fc32f4e271f4f3a59ef701c2409dfc
Nea18 <nanou718@hotmail.fr>
2019-08-02 19:16:28 +0200

    Mise en place de l'upload image simple

--- src/Entity/Parc.php
+++ src/Entity/Parc.php
@@ -2,14 +2,21 @@
 
 namespace App\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Parc;
 use Cocur\Slugify\Slugify;
+use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation\Uploadable;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Mapping\Annotation\UploadableField;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ParcRepository")
  * @ORM\HasLifecycleCallbacks
+ * @Vich\Uploadable
  */
 class Parc
 {
@@ -19,6 +26,21 @@ class Parc
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     *
+     * @var string|null
+     * @ORM\Column(type="string", length=255)
+     *
+     */
+    private $filename;
+
+    /**
+     * @var File|null
+     * @Vich\UploadableField(mapping="parc_image", fileNameProperty="filename")
+     */
+    private $imageFile;
+
 
     /**
      * @Assert\Length(min=5, max=255)
@@ -42,6 +64,11 @@ class Parc
      * @ORM\Column(type="text")
      */
     private $description;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updated_at;
 
     /**
      * Gestion automatique de nos slugs
@@ -108,6 +135,63 @@ class Parc
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     *
+     * @return string|null
+     */
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    /**
+     *
+     * @param null|string $filename
+     * @return Parc
+     */
+    public function setFilename(?string $filename): ?Parc
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    /**
+     *
+     * @return null|File
+     */
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     *
+     * @param null|File $imageFile
+     * @return Parc
+     */
+    public function setImageFile(?File $imageFile): ?Parc
+    {
+        $this->imageFile = $imageFile;
+
+        if ($this->imageFile instanceof UploadedFile) {
+            $this->updated_at = new \DateTime('now');
+        }
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
 
         return $this;
     }
--- src/Form/ParcType.php
+++ src/Form/ParcType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
@@ -35,6 +36,9 @@ class ParcType extends AbstractType
             ->add('slug', TextType::class, $this->getConfiguration("Adresse web créee automatiquement", [
                 'required' => false
             ]))
+            ->add('imageFile', FileType::class, [
+                'required' => false,
+            ])
             ->add('introduction', TextareaType::class, $this->getConfiguration("Introduction"))
             ->add('description', TextareaType::class, $this->getConfiguration("Description détaillée"))
         ;
