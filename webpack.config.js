ed2f7e2313335f6229b17743ac00f08cbebbefcb
Nea18 <nanou718@hotmail.fr>
2019-08-08 16:34:15 +0200

    Test fix 1 webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ Encore
   // public path used by the web server to access the output path
   .setPublicPath('/build')
   // only needed for CDN's or sub-directory deploy
-  //.setManifestKeyPrefix('build/')
+  .setManifestKeyPrefix('build/')
 
   /*
    * ENTRY CONFIG
@@ -23,7 +23,8 @@ Encore
    * Each entry will result in one JavaScript file (e.g. app.js)
    * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
    */
-  .addEntry('app', './assets/js/app.js')
+  .addEntry('js/app', './assets/js/app.js')
+  .addStyleEntry('css/app', './assets/css/app.css')
   //.addEntry('page1', './assets/js/page1.js')
   //.addEntry('page2', './assets/js/page2.js')
 
